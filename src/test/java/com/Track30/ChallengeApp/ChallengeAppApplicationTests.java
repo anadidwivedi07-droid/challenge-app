57f89b5177ce2ddf4d15db86447b4490f2b90197
anadidwivedi07 <anadidwivedi07@gmail.com>
2026-04-29 16:12:26 +0530

    Initial commit

---
+++ src/test/java/com/Track30/ChallengeApp/ChallengeAppApplicationTests.java
@@ -0,0 +1,12 @@
+package com.Track30.ChallengeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
